a555fe6394e9c2d1ddba83341ba6213ac972cdae
Josie DeRosier <s209055@students.aie.edu>
2021-12-15 16:56:38 -0800

    Added the basic timer and torch scripts, and a visual slider to show the status of the torch.

--- AIETeamIceWinterJam/Assets/Scripts/Timer.cs
+++ AIETeamIceWinterJam/Assets/Scripts/Timer.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float worldTimer;   // how much time has passed sicne the start of the game
+    float torchTimer;   // how much time is left before the torch goes out
+    // TODO add player controller script to reference if the player is sprinting
+
+    // public TMP_Text torchTimeRemaining;
+    public Slider torchRemainingSlider;
+
+    public float torchSpeedMod;     // the modifier that changes how fast the torch goes out
+
     void Start()
     {
-        
+        worldTimer = 0.0f;
+        torchTimer = 10.0f;
+
+        torchSpeedMod = 1.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        worldTimer += Time.deltaTime;
         
+        // TODO if player is sprinting, adjust torchSpeedMod, else put it back to 1.0f
+        torchTimer -= Time.deltaTime * torchSpeedMod;
+        torchRemainingSlider.value = torchTimer;
     }
 }
