9da57d7e2ad666c49955f5681d4e290b44fe4fb0
Josie DeRosier <s209055@students.aie.edu>
2021-12-16 12:17:57 -0800

    Main Menu all but done, will need to change Play button functionality later.

--- AIETeamIceWinterJam/Assets/Scripts/MainMenu.cs
+++ AIETeamIceWinterJam/Assets/Scripts/MainMenu.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Buttons")]
     public Button playButton;
-    public Button instructButton;
+    public Button tutorialButton;
     public Button creditsButton;
     public Button quitButton;
+    [Header("Labels")]
+    public TMP_Text label;
+    public string mainLabel;
+    public string tutorialLabel;
+    public string creditsLabel;
+    [Header("Screens")]
+    public GameObject main;
+    public GameObject tutorial;
+    public GameObject credits;
 
     void Start()
     {
-        
+        label.text = mainLabel;
+        main.SetActive(true);
+        tutorial.SetActive(false);
+        credits.SetActive(false);
     }
 
     void Update()
@@ -27,10 +41,32 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene(sceneName);
     }
 
+    public void ToTutorial()
+    {
+        label.text = tutorialLabel;
+        main.SetActive(false);
+        tutorial.SetActive(true);
+    }
+    public void ToCredits()
+    {
+        label.text = creditsLabel;
+        main.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        label.text = mainLabel;
+        main.SetActive(true);
+        tutorial.SetActive(false);
+        credits.SetActive(false);
+    }
+
     // TODO* implement are you sure?
 
     public void QuitGame()
     {
+        Debug.Log("Game Will Close");
         Application.Quit();
     }
 }
